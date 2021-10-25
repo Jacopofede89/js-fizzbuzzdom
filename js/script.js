aa72d3fc983244e731b354adc7f3ce44f1909173
Jacopofede89 <jacopofede89@gmail.com>
2021-10-25 19:55:16 +0200

    finale

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
 const doomCont = document.getElementById("containerDoom");
 
-for (let i = 1; i <= 100; i++, msg="") {
+for (let i = 1; i <= 100; i++) {
     
     let square = document.createElement("div");
     square.classList.add("square");
     square.append(i);
-    doomCont.append(square);
-    
+    doomCont.append(square);    
     console.log(doomCont);
 
     if ((i % 3 == 0) && (i % 5 == 0)) {
         console.log("FizzBuzz");
         square.classList.add('fizzbuzz');
+        square.innerHTML = "fizzbuzz";
     } else if(i % 3 == 0){
         console.log("Fizz");
         square.classList.add('fizz');
+        square.innerHTML = "fizz";
     } else if(i % 5 == 0){
         console.log("Buzz");
         square.classList.add('buzz');
+        square.innerHTML = "buzz";
     } else {
         console.log(i);
     };
